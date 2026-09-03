433a70c15b1ad3083125aff7c41f2d31e0704374
JonyLax <jonylax@gmail.com>
2026-09-03 19:12:39 +0200

    Update migrate.sql

--- migrate.sql
+++ migrate.sql
@@ -2,3 +2,4 @@ ALTER TABLE users ADD COLUMN username_changed_at INTEGER;
 ALTER TABLE users ADD COLUMN lang TEXT;
 ALTER TABLE tokens ADD COLUMN payload TEXT;
 ALTER TABLE users ADD COLUMN tour_visto INTEGER;
+ALTER TABLE users ADD COLUMN plan TEXT;
